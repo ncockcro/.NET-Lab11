330d93e38f2d114691b68ce95ba32a66d39f4aa1
ncockcro <35208141+ncockcro@users.noreply.github.com>
2018-05-14 13:44:59 -0400

    Completed Lab 11
    Full implementation for graphing a SINE wave based on the user's values.

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Lab11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fromLabel = new System.Windows.Forms.Label();
+            this.toLabel = new System.Windows.Forms.Label();
+            this.fromTextBox = new System.Windows.Forms.TextBox();
+            this.toTextBox = new System.Windows.Forms.TextBox();
+            this.fromGroupBox = new System.Windows.Forms.GroupBox();
+            this.fromEnterLabel = new System.Windows.Forms.Label();
+            this.toGroupBox = new System.Windows.Forms.GroupBox();
+            this.toEnterLabel = new System.Windows.Forms.Label();
+            this.fromGroupBox.SuspendLayout();
+            this.toGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fromLabel
+            // 
+            this.fromLabel.AutoSize = true;
+            this.fromLabel.Location = new System.Drawing.Point(34, 65);
+            this.fromLabel.Name = "fromLabel";
+            this.fromLabel.Size = new System.Drawing.Size(61, 25);
+            this.fromLabel.TabIndex = 2;
+            this.fromLabel.Text = "From";
+            this.fromLabel.Click += new System.EventHandler(this.fromLabel_Click);
+            // 
+            // toLabel
+            // 
+            this.toLabel.AutoSize = true;
+            this.toLabel.Location = new System.Drawing.Point(20, 55);
+            this.toLabel.Name = "toLabel";
+            this.toLabel.Size = new System.Drawing.Size(37, 25);
+            this.toLabel.TabIndex = 3;
+            this.toLabel.Text = "To";
+            // 
+            // fromTextBox
+            // 
+            this.fromTextBox.Location = new System.Drawing.Point(142, 59);
+            this.fromTextBox.Name = "fromTextBox";
+            this.fromTextBox.Size = new System.Drawing.Size(100, 31);
+            this.fromTextBox.TabIndex = 4;
+            this.fromTextBox.TextChanged += new System.EventHandler(this.fromTextBox_TextChanged);
+            this.fromTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.fromTextBox_keyDown);
+            // 
+            // toTextBox
+            // 
+            this.toTextBox.Location = new System.Drawing.Point(114, 55);
+            this.toTextBox.Name = "toTextBox";
+            this.toTextBox.Size = new System.Drawing.Size(100, 31);
+            this.toTextBox.TabIndex = 5;
+            this.toTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.toTextBox_keyDown);
+            // 
+            // fromGroupBox
+            // 
+            this.fromGroupBox.Controls.Add(this.fromEnterLabel);
+            this.fromGroupBox.Controls.Add(this.fromLabel);
+            this.fromGroupBox.Controls.Add(this.fromTextBox);
+            this.fromGroupBox.Location = new System.Drawing.Point(291, 38);
+            this.fromGroupBox.Name = "fromGroupBox";
+            this.fromGroupBox.Size = new System.Drawing.Size(296, 162);
+            this.fromGroupBox.TabIndex = 6;
+            this.fromGroupBox.TabStop = false;
+            this.fromGroupBox.Text = "Beginning sine value";
+            // 
+            // fromEnterLabel
+            // 
+            this.fromEnterLabel.AutoSize = true;
+            this.fromEnterLabel.Location = new System.Drawing.Point(39, 110);
+            this.fromEnterLabel.Name = "fromEnterLabel";
+            this.fromEnterLabel.Size = new System.Drawing.Size(187, 25);
+            this.fromEnterLabel.TabIndex = 5;
+            this.fromEnterLabel.Text = "Must press \"enter\"";
+            // 
+            // toGroupBox
+            // 
+            this.toGroupBox.Controls.Add(this.toEnterLabel);
+            this.toGroupBox.Controls.Add(this.toLabel);
+            this.toGroupBox.Controls.Add(this.toTextBox);
+            this.toGroupBox.Location = new System.Drawing.Point(839, 48);
+            this.toGroupBox.Name = "toGroupBox";
+            this.toGroupBox.Size = new System.Drawing.Size(240, 152);
+            this.toGroupBox.TabIndex = 7;
+            this.toGroupBox.TabStop = false;
+            this.toGroupBox.Text = "Ending sine value";
+            // 
+            // toEnterLabel
+            // 
+            this.toEnterLabel.AutoSize = true;
+            this.toEnterLabel.Location = new System.Drawing.Point(25, 100);
+            this.toEnterLabel.Name = "toEnterLabel";
+            this.toEnterLabel.Size = new System.Drawing.Size(187, 25);
+            this.toEnterLabel.TabIndex = 6;
+            this.toEnterLabel.Text = "Must press \"enter\"";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2076, 975);
+            this.Controls.Add(this.toGroupBox);
+            this.Controls.Add(this.fromGroupBox);
+            this.Name = "Form1";
+            this.Text = "Lab 11";
+            this.fromGroupBox.ResumeLayout(false);
+            this.fromGroupBox.PerformLayout();
+            this.toGroupBox.ResumeLayout(false);
+            this.toGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label fromLabel;
+        private System.Windows.Forms.Label toLabel;
+        private System.Windows.Forms.TextBox fromTextBox;
+        private System.Windows.Forms.TextBox toTextBox;
+        private System.Windows.Forms.GroupBox fromGroupBox;
+        private System.Windows.Forms.GroupBox toGroupBox;
+        private System.Windows.Forms.Label fromEnterLabel;
+        private System.Windows.Forms.Label toEnterLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,248 @@
+﻿/*
+ * Written by: Nicholas Cockcroft
+ * Date: April 10, 2018
+ * Course: .NET Environment
+ * Assignment: Lab 11
+ * 
+ * Description: Write a program to graph the SINE function for a range of values specified by the user. 
+ * (The range is a start and end x value for the graph.)  The graph you draw of the sine function should 
+ * look smooth.  Make sure your graph does not overlap the range text box. 
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab11
+{
+    public partial class Form1 : Form
+    {
+
+        // Global variables used throughout
+        int fromNumCheck = 0; // Checks to ensure the user pressed enter for from text box
+        int toNumCheck = 0;  // Checks to ensure the user pressed enter for to text box
+        float sine_start = 0f;
+        float sine_end = 0f;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics graphicsObject = e.Graphics;
+
+
+            base.OnPaint(e);
+
+            // create text brush
+            SolidBrush textBrush = new SolidBrush(Color.Black);
+
+            // create solid brush
+            SolidBrush brush = new SolidBrush(Color.White);
+
+            // draw white background
+            // x1 = 50 y1 = 120          width = 750 height = 300
+            graphicsObject.FillRectangle(brush, 0, 120, 750, 300);
+
+            // Drawing the 0 at the center of the screen
+            graphicsObject.DrawString("0", this.Font, textBrush, 350, 275);
+
+            // Get the length of how long
+            float length = sine_end - sine_start;
+
+            drawAxes(graphicsObject);
+
+            // Making sure the length is not negative
+            if(length < 0)
+            {
+                MessageBox.Show("To clause is before the from clause. Can not graph sine wave.");
+                return;
+            }
+
+            // Need to allocate 10 times the length for the size of the array since
+            // I will be incrementing by 0.1 each time to store a new point later
+
+            double[] x = new double[Convert.ToInt32(length) * 10];
+            double[] y = new double[Convert.ToInt32(length) * 10];
+
+
+            // If the user has entered two numbers...
+            if (fromNumCheck > 0 && toNumCheck > 0)
+            {
+
+                double increment = 0.0;
+                for (int i = 0; i < length * 10; i++)
+                {
+                    // Store the x value and add 350 so it starts at 0 and the y value which is just the sine of original x
+                    try
+                    {
+                        x[i] = sine_start + increment + 350;
+                        y[i] = Math.Sin(sine_start + increment);
+                    }
+                    catch(Exception)
+                    {
+                        continue;
+                    }
+
+                    // Incrementing by 0.1 each time to make a very "smooth" curve
+                    increment = increment + 0.1;
+                }
+                
+            }
+
+            // Send over the x and y arrays, graphics object, and length the user specified to be drawn
+            graphPoints(x, y, graphicsObject, length);
+
+
+        }
+
+        // Graphs the x and y axis as well as the sine graph
+        void graphPoints(double[] x, double[] y, Graphics g, float length)
+        {
+            // Uses anti aliasing to make the graph look a little more smooth
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            // Creating a blue pen to be used for drawing the sine waves
+            Pen bluePen = new Pen(Color.Black, 1 / g.DpiX);
+
+            // Ensuring the user typed in 2 numbers...
+            if (fromNumCheck > 0 && toNumCheck > 0)
+            {
+                Size size = new Size(750, 300);
+
+                // Get the max and min x and y values.
+                double maxX = maxValue(x);
+                double maxY = maxValue(y);
+                double minX = minValue(x);
+                double minY = minValue(y);
+
+                // Creating new arrays for where the pixel values will go for x and y
+                double[] px = new double[x.Length];
+                double[] py = new double[y.Length];
+
+                // Scale the points to pixels.
+                for (int ip = 0; ip < x.Length; ip++)
+                {
+                    px[ip] = (x[ip] - minX) / (maxX - minX) * (size.Width - sine_start - 375) + Math.Abs(x[ip]);
+                    py[ip] = size.Height - (y[ip] - minY) / (maxY - minY) * 100 + 28;
+                }
+
+
+                // Draw all of the lines from points
+                for (int ip = 1; ip < x.Length; ip++)
+                {
+                    if (px[ip] < 750)
+                    {
+                        g.DrawLine(bluePen, (float)px[ip - 1], (float)py[ip - 1], (float)px[ip], (float)py[ip]);
+                    }
+                }
+            }
+
+        }
+
+        void drawAxes(Graphics g)
+        {
+            // Using a blue pen for drawing the axes
+            Pen bluePen = new Pen(Color.Black, 1 / g.DpiX);
+
+            // Values used for drawing x and y axis
+            float x0 = 0;
+            float y0 = 200f;
+
+            using (Pen p = new Pen(Color.Blue))
+
+            {
+                //Draw X-coordinate
+                g.DrawLine(p, x0, y0 + 75, x0 + 750, y0 + 75);
+
+                //Draw Y-coordinate
+                g.DrawLine(p, x0 + 350, y0 + 220, x0 + 350, y0 - 80);
+
+            }
+        }
+
+        // Gets the maximum values from a list of points
+        double maxValue(double[] x)
+        {
+            double max = x[0];
+            foreach (double a in x)
+            {
+                if (max < a) max = a;
+            }
+            return max;
+        }
+        // Gets the minimum values from a list of points
+        double minValue(double[] x)
+        {
+            double min = x[0];
+            foreach (double a in x)
+            {
+                if (min > a) min = a;
+            }
+            return min;
+        }
+        private void fromTextBox_keyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.KeyCode != Keys.Enter)
+                {
+                    return;
+                }
+
+                sine_start = float.Parse(fromTextBox.Text);
+                fromNumCheck++;
+
+                Invalidate();
+            }
+            catch(Exception)
+            {
+                return;
+            }
+
+            
+        }
+
+        private void toTextBox_keyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.KeyCode != Keys.Enter)
+                {
+                    return;
+                }
+
+                sine_end = float.Parse(toTextBox.Text);
+                toNumCheck++;
+
+                Invalidate();
+            }
+            catch(Exception)
+            {
+                return;
+            }
+
+
+        }
+
+        private void fromTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fromLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
